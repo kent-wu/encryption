7461afe2093424073d383ada52a6dd7321036e24
Junhong Wu <jhwu@thoughtworks.com>
2017-05-05 10:40:13 +0800

    refactor

--- src/main/java/com/huawei/it/Encryption.java
+++ src/main/java/com/huawei/it/Encryption.java
@@ -6,19 +6,36 @@ public class Encryption {
     //This is the key to encrypt or decrypt,
     //You should change it.
     //The length of key should be 16.
-    private static String key="keyShould16Chars";
-    
-    public static void main(String[] args) throws Exception {
-        System.out.println("------等待输入,输入密码后按回车:-------");
-        Console console = System.console();
-        char[] password = console.readPassword();
-        String pwd = String.valueOf(password);
-        String encrypt = encrypt(new String[]{pwd});
+    private static String key = "keyShould16Chars";
 
-        System.out.println("-------成功-------");
+    public static void main(String[] args) throws Exception {
+        Boolean isPwdMatch = false;
+        String password = "";
+
+        while (!isPwdMatch) {
+            System.out.println("------等待输入,输入密码后按回车:-------");
+            Console console = System.console();
+            char[] inputPassword1 = console.readPassword();
+            String pwd = String.valueOf(inputPassword1);
+
+            System.out.println("------请再次输入密码,之后按回车:-------");
+            char[] passwordConfirm = console.readPassword();
+            String pwdConfirm = String.valueOf(passwordConfirm);
+
+            if (!pwd.equals(pwdConfirm)) {
+                System.out.println("###### 两次密码不一致，请再次输入 ######");
+                continue;
+            }
+            password = pwdConfirm;
+            isPwdMatch = true;
+        }
+
+        String encrypt = encrypt(new String[]{password});
+
+        System.out.println("###### 成功 ######");
         System.out.print("输入:");
         StringBuilder builder = new StringBuilder();
-        for (int i = 0; i < password.length; i++) {
+        for (int i = 0; i < password.length(); i++) {
             builder.append("*");
         }
         System.out.println(builder.toString());
